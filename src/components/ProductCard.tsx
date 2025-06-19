643340965548a6f890e0de237c19a8f4b83b74c1
Jemima Jange <jemimajange@gmail.com>
2025-06-19 18:24:21 -0500

    feat: update mobile

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -17,14 +17,14 @@ export default function ProductCards() {
       </div>
 
       {/* CAPITAL STAKING X */}
-      <div className="absolute top-[29%] right-[4%] w-64">
+      <div className="absolute top-[27%] right-[4%] w-64">
         <div className="relative">
           <img 
-            src="/img/crypto.png" 
+            src="/img/CAPITAL-STAKING 1.png" 
             alt="CAPITAL STAKING X" 
-            className="absolute top-0 right-1 w-28 h-15 mb-2"
+            className="absolute top-1 right-1 w-36 h-23 mb-2"
           />
-          <p className="text-sm px-2 text-gray-200 mb-0 absolute top-15 right-0 text-right">
+          <p className="text-sm px-2 text-gray-200 mb-0 absolute top-21 right-0 text-right">
             <b>CAPITAL STAKING X</b> Tu bienestar financiero empieza con
             Blockchain real.
           </p>
@@ -32,28 +32,28 @@ export default function ProductCards() {
       </div>
 
       {/* Capital Crypto X */}
-      <div className="absolute top-[45%] right-[25%] w-64">
+      <div className="absolute top-[43%] right-[25%] w-64">
         <div className="relative">
           <img 
-            src="/img/crypto.png" 
+            src="/img/CAPITAL-CRYPTO 1.png" 
             alt="Capital Crypto X" 
-            className="absolute top-0 left-0 w-28 h-15 mb-2"
+            className="absolute top-0 left-0 w-30 h-20 mb-2"
           />
-          <p className="text-sm text-gray-200 mb-0 absolute top-15 left-0">
+          <p className="text-sm text-gray-200 mb-0 absolute top-20 left-0">
             <b>Capital Crypto X</b> Compra, vende y envía cripto al instante.
           </p>
         </div>
       </div>
 
       {/* INFINITY ACADEMY */}
-      <div className="absolute top-[60%] left-[10%] w-64">
+      <div className="absolute top-[58%] left-[24%] w-64">
         <div className="relative">
           <img 
-            src="/img/crypto.png" 
+            src="/img/INFINITY-ACADEMY 1.png" 
             alt="INFINITY ACADEMY" 
-            className="absolute top-0 right-0 w-45 h-45"
+            className="absolute -top-0 right-0 left-26 w-45 h-25"
           />
-          <p className="text-sm text-gray-200 mb-0 absolute top-35 right-1 text-right">
+          <p className="text-sm text-gray-200 mb-0 absolute top-20 right-1 text-right">
             <b>INFINITY ACADEMY</b> Domina los mercados, aprende Trading y
             controla tus emociones
           </p>
@@ -64,7 +64,7 @@ export default function ProductCards() {
       <div className="absolute top-[75%] right-[10%] w-64">
         <div className="relative">
           <img 
-            src="/img/crypto.png" 
+            src="/img/CAPITAL-CASHX 1.png" 
             alt="Capital CashX" 
             className="absolute top-0 left-0 w-45 h-45"
           />
