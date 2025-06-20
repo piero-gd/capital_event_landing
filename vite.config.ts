1c344c193ac6f93e5f06e7dd701de0cc6e2fd655
Jemima Jange <jemimajange@gmail.com>
2025-06-19 21:16:42 -0500

    fix: saltear tailwind por errores

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+import { defineConfig } from 'vite';
+
+// @ts-ignore - Ignorar temporalmente el error de tipo
+export default defineConfig(async () => {
+  // @ts-ignore - Ignorar temporalmente el error de tipo
+  const tailwindcss = (await import('@tailwindcss/vite')).default;
+  
+  return {
+    plugins: [
+      tailwindcss(),
+    ],
+  };
+});
